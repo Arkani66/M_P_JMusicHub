7906f35930d9d7efcedccfde57dc9341445246d8
PROSPA Florence <74834074+Arkani66@users.noreply.github.com>
2021-05-03 15:42:50 +0200

    add compilation files

---
+++ compile.bat
@@ -0,0 +1,3 @@
+set JAVA_HOME=C:\Programme Files\Java\jdk-12.0.2
+
+javac src\musichub\business\*.java src\musichub\util\*.java src\musichub\main\*.java -d bin
---
+++ run.bat
@@ -0,0 +1,3 @@
+set JAVA_HOME=C:\Program Files\Java\jdk-12.0.2
+
+java -classpath bin musichub.main.MusicHubConsole
